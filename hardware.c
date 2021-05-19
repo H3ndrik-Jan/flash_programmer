27210e5806558d07a4b4b16e8aa444675c2ac407
Hendrik-Jan Kuijt <hendrik-jan.kuijt@hva.nl>
2021-05-19 22:32:28 +0200

    spi 100ns delay

--- hardware.c
+++ hardware.c
@@ -1,6 +1,9 @@
 #include "hardware.h"
 #include "BCM2711macros.h"
 
+#define SPISLEEP_NS 100
+
+
 void setupFlashProgrammer(void){
 	
 	setup_io();
@@ -53,16 +56,18 @@ void writeCS(bool state){
 uint8_t softSpiTransfer(uint8_t outByte){
 	uint8_t inByte = 0;
 	
+	timespec delayTime = {0};
+	delayTime.tv_nsec = SPISLEEP_NS;
 	//read current value
 	for(uint8_t index = 0; index<8; index++){
-		if((outByte>>index) & 0x01) GPIO_SET = 1<<PINSO;
-		else GPIO_CLR = 1<<PINSO;
+		if((outByte>>index) & 0x01) GPIO_SET = 1<<PINSI;
+		else GPIO_CLR = 1<<PINSI;
 
 		GPIO_CLR = 1<<PINCLK;
-		usleep(1);
+		nanosleep(&delayTime, NULL);
 		GPIO_SET = 1<<PINCLK;
-		usleep(1);
-		inByte |= (GET_GPIO(PINSI)?0x01:0x00)<<index;
+		nanosleep(&delayTime, NULL);
+		inByte |= (GET_GPIO(PINSO)?0x01:0x00)<<index;
 	}
 	
 	return inByte;
